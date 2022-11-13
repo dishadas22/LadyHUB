2f3eb1e23558fd21547315f1845cb024b49188cf
dishadas22 <115528313+dishadas22@users.noreply.github.com>
2022-11-13 11:40:17 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     
-$server= "localhost";
-$username="root";
-$password="";
-$con= mysqli_connect($server,$username,$password,'sashakti');
+$server= "remotemysql.com";
+$username="9rUhwdd4fH";
+$password="hRxZhxvv2G";
+$con= mysqli_connect($server,$username,$password,'9rUhwdd4fH');
 if(!$con){
     die("Connection not made due to".mqsqli_connect_error());
 }
